c3b3ed05614bda6939f40368374b1618f9bbb963
Travis CI User <travis@example.org>
2020-11-12 17:58:13 +0000

    [ci skip] update gh-pages

--- build..alpha..travis-ci.com/app/assets.utility2.rollup.js
+++ build..alpha..travis-ci.com/app/assets.utility2.rollup.js
@@ -1545,7 +1545,7 @@ if (module === require.main && !globalThis.utility2_rollup) {
 /* script-begin /assets.utility2.lib.istanbul.js */
 // usr/bin/env node
 /*
- * lib.istanbul.js (2020.8.1)
+ * lib.istanbul.js (2020.11.12)
  * https://github.com/kaizhu256/node-istanbul-lite
  * this zero-dependency package will provide browser-compatible version of istanbul coverage-tool (v0.4.5), with working web-demo
  *
@@ -31328,7 +31328,7 @@ if (local.isBrowser) {
 /* script-begin /assets.utility2.js */
 // usr/bin/env node
 /*
- * lib.utility2.js (2020.11.3)
+ * lib.utility2.js (2020.11.12)
  * https://github.com/kaizhu256/node-utility2
  * this zero-dependency package will provide high-level functions to to build, test, and deploy webapps
  *
